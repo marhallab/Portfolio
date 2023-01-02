00b8925a94ff28aeb852e788b4f5772c266dd74f
Med Amine Rhallab <rhallab.dev@gmail.com>
2023-01-02 17:45:07 -0500

    Optimize and organise code

---
+++ src/static/scripts/Skills.js
@@ -0,0 +1,82 @@
+export const mySkills = [
+    {
+        name: "HTML 5",
+        icon: "images/skills/html5.png"
+    },
+
+    {
+        name: "CSS 3",
+        icon: "images/skills/css3.png"
+    },
+
+    {
+        name: "Javascript",
+        icon: "images/skills/javascript.png"
+    },
+
+    {
+        name: "VueJs (2 & 3)",
+        icon: "images/skills/vuejs.png"
+    },
+
+    {
+        name: "Rest Api",
+        icon: "images/skills/rest-api.png"
+    },
+
+    {
+        name: "JQuery",
+        icon: "images/skills/jquery.png"
+    },
+
+    {
+        name: "Django",
+        icon: "images/skills/django.png"
+    },
+
+    {
+        name: "Sass",
+        icon: "images/skills/sass.png"
+    },
+
+    {
+        name: "Tailwind",
+        icon: "images/skills/tailwind.png"
+    },
+
+    {
+        name: "Bootstrap",
+        icon: "images/skills/bootstrap.png"
+    },
+
+    {
+        name: "Git",
+        icon: "images/skills/git.png"
+    },
+
+    {
+        name: "Scrum",
+        icon: "images/skills/sprint-iteration.png"
+    },
+
+    {
+        name: "Seo",
+        icon: "images/skills/seo.png"
+    },
+
+    {
+        name: "Wordpress",
+        icon: "images/skills/wordpress.png"
+    },
+
+    {
+        name: "Responsive Design",
+        icon: "images/skills/responsive-design.png"
+    },
+
+    {
+        name: "Web Accessibility",
+        icon: "images/skills/web-accessibility.png"
+    },
+
+]
