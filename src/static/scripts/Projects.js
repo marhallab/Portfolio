c3acca811ab5bbaf0923191117e1ae64851bf476
Med Amine Rhallab <rhallab.dev@gmail.com>
2023-02-28 19:12:38 -0500

    Rezise images

--- src/static/scripts/Projects.js
+++ src/static/scripts/Projects.js
@@ -2,7 +2,7 @@ export const myProjects = [
     {
         name: "Journal de l'assurance",
         description: isEn ? "<p class='mb-2'>JDA is a nation wide renowed information source for profesionnal in the insurance industrie.</p><p class='mb-2'>With the objectives of transitionning the front-end from Django to a Vue envirement, i worked on optimisation, refractoring and creating multiple new applications and features on the main project.</p><p>From refractoring the dynamic event app to creating a new subscription flow with stripe, this is only the tip of the iceberg.</p>" : "<p class='mb-2'>JDA est une source d'information reconnue à l'échelle nationale pour les professionnels du secteur de l'assurance.</p><p class='mb-2'>Avec les objectifs de transition du front-end de Django dans un environnement Vue, j'ai travaillé sur l'optimisation, la réfraction et la création de plusieurs nouvelles applications et fonctionnalités sur le projet principal.</p><p>De la réfraction de l'application d'événement dynamique à la création d'un nouveau flux d'abonnement avec Stripe, ce n'est que le conseil de l'iceberg.</p>",
-        image: "images/projects/jda.png",
+        image: "images/projects/jda.webp",
         technologies: [
             {
                 name: "HTML 5",
@@ -55,7 +55,7 @@ export const myProjects = [
     {
         name: "My personal Portfolio",
         description: isEn ? "<p class='mb-2'>Well yes, this very website is made by.... ME! Surprise!</p><p class='mb-2'>The website is done with Vue 3, using vitejs. I had some fun using mutiple external js libraries for some cool visuel effects.</p><p class='mb-2'> As for the contact form, i'm using EmailJS to receive the emails from my code.</p> <p class='mb-6'>This website is deployed and hosted with netlify</p>" : "<p class='mb-2'>Eh bien oui, ce site est fait par... MOI ! Surprise !</p><p class='mb-2'>Le site est fait avec Vue 3, en utilisant vitejs. Je me suis amusé à utiliser plusieurs bibliothèques js externes pour des effets visuels sympas.</p> <p class='mb-2'> En ce qui concerne le formulaire de contact, j'utilise EmailJS pour recevoir les e-mails de mon code.</ p> <p class='mb-6'>Ce site Web est déployé et hébergé avec netlify</p>",
-        image: "images/projects/portfolio.png",
+        image: "images/projects/portfolio.webp",
         technologies: [
             {
                 name: "HTML 5",
@@ -107,7 +107,7 @@ export const myProjects = [
     {
         name: "Alfred Technologies",
         description: isEn ? "<p class='mb-2'>A website which serves as an initial introduction to the alfred management software. We used Vuejs and wordpress as a headless CMS.</p><p>I also added multiple new components created in the website to the UI-Kit used internally.</p>" : "<p class='mb-2'>Un site Web qui sert de première introduction au logiciel de gestion alfred. Nous avons utilisé Vuejs et wordpress comme CMS em headless.</p><p>J'ai également ajouté plusieurs nouveaux composants créés sur le site Web au UI kit utilisé en interne.</p>",
-        image: "images/projects/alfred.png",
+        image: "images/projects/alfred.webp",
         technologies: [
             {
                 name: "HTML 5",
